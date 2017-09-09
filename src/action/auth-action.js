b2b0a2b785c7b0da49a51c55f1438ff478a30a34
Eddie <Eddie@Edwins-MacBook-Pro.local>
2017-09-09 10:24:15 -0700

    Wrote action for user authentication

---
+++ src/action/auth-action.js
@@ -0,0 +1,40 @@
+import superagent from 'superagent';
+
+export const tokenSet = token => ({
+  type: 'TOKEN_SET',
+  payload: token
+});
+
+export const tokenDelete = () => ({
+  type: 'TOKEN_DELETE'
+});
+
+export const signupRequest = user => dispatch => {
+  return superagent.post(`${__API_URL__}/api/signup`)
+  .withCredentials()
+  .send(user)
+  .then(res => {
+    dispatch(tokenSet(res.text))
+    try{
+      localStorage.auth = res.text;
+    } catch(err) {
+      console.error(err);
+    }
+    return res;
+  })
+}
+
+export const loginRequest = user => dispatch => {
+  return superagent.get(`${__dirname}/api/login`)
+  .withCredentials()
+  .set(user.userName, user.passWord)
+  .then(res => {
+    dispatch(tokenSet(res.text))
+    try {
+      localStorage.auth = res.text;
+    } catch(err) {
+      console.error(err);
+    }
+    return res;
+  })
+}
