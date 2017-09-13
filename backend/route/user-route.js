5e9a049e5f62c574a731762e010bfdf6ef8327df
Luis Manzo <manzosk3@gmail.com>
2017-09-12 23:10:56 -0700

    sets up oauth for app

--- backend/route/user-route.js
+++ backend/route/user-route.js
@@ -4,12 +4,50 @@ const Router = require('express').Router;
 const debug = require('debug')('giggle: User Router');
 const createError = require('http-errors');
 const jasonParser = require('body-parser').json();
+const superagent = require('superagent');
 
 const User = require('../model/user.js');
 const Profile = require('../model/profile.js');
 const basicAuth = require('../lib/basic.js');
 
 const userRouter = module.exports = new Router();
+
+
+userRouter.get('/oauth/google/code', (req, res, next) => {
+  if (!req.query.code) {
+    res.redirect(process.env.CLIENT_URL);
+  } else {
+    superagent.post('https://www.googleapis.com/oauth2/v4/token')
+    .type('form')
+    .send({
+      code: req.query.code,
+      grant_type: 'authorization_code',
+      client_id: process.env.GOOGLE_CLIENT_ID,
+      client_secret: process.env.GOOGLE_CLIENT_SECRET,
+      redirect_uri: `${process.env.API_URL}/oauth/google/code`
+    })
+    .then(response => {
+      console.log('POST: oauth2/v4/token', response.body);
+      return superagent.get('https://www.googleapis.com/plus/v1/people/me/openIdConnect')
+      .set('Authorization', `Bearer ${response.body.access_token}`)
+    })
+    .then(response => {
+      console.log('GET: /people/me/openIdConnect', response.body);
+      console.log('response body', response.body)
+      return User.handleOAUTH(response.body);
+    })
+    .then(user => user.tokenCreate())
+    .then( token => {
+      console.log('my oauth token:', token);
+      res.cookie('Giggle-Token', token);
+      res.redirect(process.env.CLIENT_URL);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.redirect(process.env.CLIENT_URL);
+    })
+  }
+})
 
 userRouter.post('/api/signup', jasonParser, function(req, res, next) {
   debug('POST /api/signup');
