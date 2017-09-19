37ae64c622513e9bc18aa1f4c3b01f667e9c8a09
Eddie <Eddie@Edwins-MacBook-Pro.local>
2017-09-19 14:58:27 -0700

    Created a booking form :D

---
+++ frontend/src/component/profile-components/booking-form/index.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+class BookingForm extends React.Component {
+  constructor(props) {
+    super(props);
+    let {profile, target} = this.props;
+    let initialState = profile.type === 'band' ?
+    {
+      bandName: profile.userName,
+      venueName: target.userName,
+      coords: target.coords,
+      city: target.city,
+      state: target.state
+    }:
+    {
+      bandName: target.userName,
+      venueName: profile.userName,
+      coords: profile.coords,
+      city: profile.city,
+      state: profile.state,
+      date: new Date()
+    }
+
+    this.state = {
+      ...initialState,
+      coverCharge: 0,
+      compensation: 0,
+      description: '',
+      time: '',
+
+    }
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  onChange(e) {
+    let {name, value} = e.target;
+    console.log(value);
+    this.setState({
+      [name]: value
+    });
+  }
+
+  onSubmit() {
+
+  }
+
+  render() {
+    return(
+      <form onSubmit={this.onSubmit} id='booking-form'>
+        <p>{`Band Name: ${this.state.bandName}`}</p>
+        <p>{`Venue Name: ${this.state.venueName}`}</p>
+        <p>{`Location: ${this.state.city},${this.state.state}`}</p>
+        <p>Date:</p>
+        <input
+          type='date'
+          name='date'
+          value={this.state.date}
+          onChange={this.onChange}
+        />
+        <p>Time:</p>
+        <input
+          type='time'
+          name='time'
+          value={this.state.time}
+          onChange={this.onChange}
+        />
+        <p>Please Enter a Cover Chare:</p>
+        <input
+          type='number'
+          name='cover'
+          placeholder='0$'
+          value={this.state.cover}
+          onChange={this.state.onChange}
+        />
+        <p>Please Enter a Compensation Amount:</p>
+        <input
+          type='number'
+          name='compensation'
+          placeholder='0$'
+          value={this.state.cover}
+          onChange={this.state.onChange}
+        />
+        <p>Description:</p>
+        <textarea
+          name='description'
+          type='text'
+          placeholder='Please enter a description'
+          value={this.state.description}
+          onChange={this.state.onChange}
+        ></textarea>
+      </form>
+    )
+  }
+}
+let mapStateToProps = state => ({
+  profile: state.profile,
+  socket: state.socket
+});
+let mapDispatchToProps = dispatch => ({
+
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookingForm);
