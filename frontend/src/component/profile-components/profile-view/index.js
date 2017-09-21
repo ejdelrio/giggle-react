0d3dbce1f6be7be3caed2b0e2127e2d2a1ca81df
Luis Manzo <manzosk3@gmail.com>
2017-09-21 09:59:14 -0700

    profile-view: profile header complete

--- frontend/src/component/profile-components/profile-view/index.js
+++ frontend/src/component/profile-components/profile-view/index.js
@@ -1,7 +1,7 @@
 import './_profile-view.scss';
 import React from 'react';
-import {connect} from 'react-redux';
-import {Link} from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import superagent from 'superagent';
 
 import * as util from '../../../lib/util';
@@ -11,7 +11,7 @@ import Modal from '../../lib/modal';
 import BookingForm from '../booking-form';
 
 
-class ProfileView extends React.Component{
+class ProfileView extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -27,28 +27,29 @@ class ProfileView extends React.Component{
     console.log(this.state);
   }
   componentDidMount() {
-    let {params} = this.props.match;
+    let { params } = this.props.match;
     superagent.get(`${__API_URL__}/api/profile/${params.userName}`)
-    .end((error, res) => {
-      if(error) return this.setState({error});
-      this.setState({target: res.body});
-    })
+      .end((error, res) => {
+        if (error) return this.setState({ error });
+        this.setState({ target: res.body });
+      })
   }
 
   requestBooking() {
     let newState = this.state.bookingModalToggle ? false : true;
-    this.setState({bookingModalToggle: newState});
+    this.setState({ bookingModalToggle: newState });
   }
 
   sendMessage() {
 
   }
   render() {
-    if(!this.state.target) return (<section></section>)
+    if (!this.state.target) return (<section></section>)
     let profile = this.state.target;
-    return(
+    return (
 
       <section id='profile-view'>
+
         {util.renderIf(this.state.bookingModalToggle,
           <Modal
             closeModal={this.requestBooking}
@@ -60,38 +61,78 @@ class ProfileView extends React.Component{
             />
           </Modal>
         )}
-        <div id='profile-display'>
-          {//<img src={profile.avatar} />
-          }
-          <h4>{profile.userName}</h4>
-          <p>{profile.bio}</p>
-          <button onClick={this.requestBooking}>Request Booking</button>
-          <button onClick={this.sendMessage}>Send Message</button>
-          <p>{`Location: ${profile.city}, ${profile.state}`}</p>
-          <p>{`Genres: ${profile.genre.join(', ')}`}</p>
-          <ul>
-            <Link to={`/profile/photos-${profile.userName}`}>
-              <li>
-                <img src='https://assets3.thrillist.com/v1/image/1531235/size/tmg-slideshow_l.jpg' />
-              </li>
-            </Link>
-            <Link to={`/profile/videos-${profile.userName}`}>
-              <li>
-                <img src='' />
-              </li>
-            </Link>
-            <Link to={`/profile/tracks-${profile.userName}`}>
-              <li>
-                <img src='' />
-              </li>
-            </Link>
-          </ul>
-        </div>
-        <div id='my-bookings'>
-          <ul>
-          {//Bookigs will be mapped here :D
-          }
-          </ul>
+        <div id='profile-view'>
+          <div className="profile-header">
+
+            <div className="container">
+              <div className="ident-content">
+              <div className="circle photo-ident">
+
+                <button className="photo-update">
+                  Update image
+                </button>
+              </div>
+              <div className="text-ident">
+                <h2>{profile.userName}</h2>
+
+                <p className='city-state'>{`${profile.city}, ${profile.state}`}</p>
+              </div>
+
+              </div>
+              <div className="profile-contact">
+                <div className="contact-buttons">
+                  <button onClick={this.requestBooking}>Request Booking</button>
+                  <button onClick={this.sendMessage}>Send Message</button>
+                </div>
+
+
+              </div>
+
+
+            </div>
+
+
+
+
+            <p>{`${profile.genre.join(', ')}`}</p>
+          </div>
+
+          <div className="profile-main">
+            <div id='my-bookings'>
+              <ul>
+                {//Bookigs will be mapped here :D
+                }
+              </ul>
+            </div>
+
+            <div className="bio">
+              <h2>Bio:</h2>
+              <p>{profile.bio}</p>
+
+            </div>
+
+            <div className="media">
+
+              <h2>Media</h2>
+              <ul>
+                <Link to={`/profile/photos-${profile.userName}`}>
+                  <li>
+                    {/* <img src='https://assets3.thrillist.com/v1/image/1531235/size/tmg-slideshow_l.jpg' /> */}
+                  </li>
+                </Link>
+                <Link to={`/profile/videos-${profile.userName}`}>
+                  <li>
+                    <img src='' />
+                  </li>
+                </Link>
+                <Link to={`/profile/tracks-${profile.userName}`}>
+                  <li>
+                    <img src='' />
+                  </li>
+                </Link>
+              </ul>
+            </div>
+          </div>
         </div>
       </section>
     )
